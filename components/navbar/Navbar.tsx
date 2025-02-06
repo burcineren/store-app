7dc0c04d02d54050c2792b34c3f1df16378ff602
burcineren <burcin.ern969@gmail.com>
2025-02-06 16:30:58 +0300

    first commit

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import Container from "../global/Container";
+import CartButton from "./CartButton";
+import LinksDropdown from "./LinksDropdown";
+import Logo from "./Logo";
+import NavSearch from "./NavSearch";
+import DartMode from "./DartMode";
+
+function Navbar() {
+  return (
+    <div className="border-b">
+      <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8"></Container>
+      <Logo />
+      <NavSearch />
+      <div className="flex gap-4 items-center">
+        <CartButton />
+        <DartMode />
+        <LinksDropdown />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
