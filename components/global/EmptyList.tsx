b8a9daff30cf688fab0cbd7805201f2a45393bb8
burcineren <burcin.ern969@gmail.com>
2025-02-07 19:54:48 +0300

    feat: import seperator and fetch products

--- components/global/EmptyList.tsx
+++ components/global/EmptyList.tsx
@@ -1,5 +1,13 @@
-function EmptyList() {
-  return <div>EmptyList</div>;
+import { cn } from "@/lib/utils";
+
+function EmptyList({
+  heading = "No items found.",
+  className,
+}: {
+  heading?: string;
+  className?: string;
+}) {
+  return <div className={cn("text-xl", className)}>{heading}</div>;
 }
 
 export default EmptyList;
--- components/global/SectionTitle.tsx
+++ components/global/SectionTitle.tsx
@@ -1,5 +1,14 @@
-function SectionTitle() {
-  return <div>SectionTitle</div>;
+import { Separator } from "../ui/separator";
+
+function SectionTitle({ text }: { text: string }) {
+  return (
+    <>
+      <h2 className="text-3xl font-medium tracking-wider capitalize mb-8">
+        {text}
+      </h2>
+      <Separator />
+    </>
+  );
 }
 
 export default SectionTitle;
