f0b7c91c7cf6a50a5eb29c8f58e61fc3589fe007
burcineren <burcin.ern969@gmail.com>
2025-02-09 01:00:11 +0300

    feat: featured Products list

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,5 +1,11 @@
-function FavoriteToggleButton() {
-  return <div>FavoriteToggleButton</div>;
+import { Button } from "../ui/button";
+import { FaHeart } from "react-icons/fa";
+function FavoriteToggleButton({ productId }: { productId: string }) {
+  return (
+    <Button size="icon" variant="outline" className="p-2 cursor-pointer">
+      <FaHeart />
+    </Button>
+  );
 }
 
 export default FavoriteToggleButton;
