eeea798a7fb185ad8fa64ed2db3de3810be46a24
burcineren <burcin.ern969@gmail.com>
2025-02-10 20:18:16 +0300

    feat: hero component

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,5 +1,27 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import HeroCarousel from "./HeroCarousel";
+
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+      <div>
+        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+          We are changing the way people shop
+        </h1>
+        <p className="m-8 max-w-xl text-lg leading-8 text-muted-foreground">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
+          voluptates nesciunt vel amet iste ratione quibusdam repudiandae, alias
+          doloremque impedit dicta ut harum temporibus maiores ducimus adipisci,
+          odio enim tenetur!
+        </p>
+        <Button asChild size="lg" className="mt-10">
+          <Link href="/product">Our Products</Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 }
 
 export default Hero;
