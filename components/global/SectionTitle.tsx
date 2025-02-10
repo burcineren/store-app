52827e59d98f70aa1a0961e87e6f8db6fdf1d149
burcineren <burcin.ern969@gmail.com>
2025-02-10 21:19:14 +0300

    feat: loadingContainer

--- components/global/SectionTitle.tsx
+++ components/global/SectionTitle.tsx
@@ -3,9 +3,9 @@ import { Separator } from "../ui/separator";
 function SectionTitle({ text }: { text: string }) {
   return (
     <div>
-      <h2 className="text-3xl font-medium tracking-wider capitalize mb-8">
+      <h3 className="text-3xl font-medium tracking-wider capitalize mb-8">
         {text}
-      </h2>
+      </h3>
       <Separator />
     </div>
   );
