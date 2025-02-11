d9921216acc5941bc31d26cc4af236f3ee160392
burcineren <burcin.ern969@gmail.com>
2025-02-11 14:35:02 +0300

    feat: navbar search

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -4,13 +4,16 @@ import LinksDropdown from "./LinksDropdown";
 import Logo from "./Logo";
 import NavSearch from "./NavSearch";
 import DartMode from "./DartMode";
-
+import { Suspense } from "react";
 function Navbar() {
   return (
     <div className="border-b">
       <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8">
         <Logo />
-        <NavSearch />
+        <Suspense>
+          <NavSearch />
+        </Suspense>
+
         <div className="flex gap-4 items-center">
           <CartButton />
           <DartMode />
