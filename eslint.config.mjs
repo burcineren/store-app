64323af99b9d1bdd1e6acc5d1d7281c15ccbcef0
burcineren <burcin.ern969@gmail.com>
2025-02-13 20:41:57 +0300

    fix: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ];
