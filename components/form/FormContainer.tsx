88829f3ff0e7a0b870b08f53070a6bb63cb551f6
burcineren <burcin.ern969@gmail.com>
2025-02-18 16:14:43 +0300

    fix: form componets

--- components/form/FormContainer.tsx
+++ components/form/FormContainer.tsx
@@ -1,5 +1,28 @@
-function FormContainer() {
-  return <div>FormContainer</div>;
+"use client";
+import React, { useEffect } from "react";
+import { actionFunction } from "@/utils/types";
+import { useToast } from "@/hooks/use-toast";
+import { useFormState } from "react-dom";
+
+const initialState = {
+  message: "",
+};
+
+function FormContainer({
+  action,
+  children,
+}: {
+  action: actionFunction;
+  children: React.ReactNode;
+}) {
+  const [state, formAction] = React.useActionState(action, initialState);
+  const { toast } = useToast();
+  useEffect(() => {
+    if (state.message) {
+      toast({ description: state.message });
+    }
+  }, [state]);
+  return <form action={formAction}>{children}</form>;
 }
 
 export default FormContainer;
